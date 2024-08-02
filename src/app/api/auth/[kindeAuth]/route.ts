9edfebdcea03628e0942b0dc1455cff3759694ca
umutcan <eubalcin@gmail.com>
2024-08-02 16:02:08 +0300

    route.ts

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,8 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export const GET = handleAuth({
+  clientId: process.env.KINDE_CLIENT_ID!,
+  clientSecret: process.env.KINDE_CLIENT_SECRET!,
+  issuer: process.env.KINDE_ISSUER_URL!,
+  redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI!,
+});
