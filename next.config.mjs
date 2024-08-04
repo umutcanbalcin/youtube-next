4df0971d7a8a99d3a1aa5aac516b3f240f884812
umutcan <eubalcin@gmail.com>
2024-08-04 21:22:38 +0300

    vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', ,'lh3.googleusercontent.com', 'https://strapi-cms-7jji.onrender.com/'],
+    domains: ['localhost','lh3.googleusercontent.com', 'https://strapi-cms-7jji.onrender.com'],
   },
 };
 
